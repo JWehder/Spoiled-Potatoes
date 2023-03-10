3fe43777d8b53fd04b538f6c30d6a3ecf3139213
Jake Wehder <jw@email.comjake>
2023-03-10 14:21:05 -0600

    adding a migration to hold the password reset code as well as request time

---
+++ app/mailers/password_reset_mailer.rb
@@ -0,0 +1,20 @@
+class PasswordResetMailer < ApplicationMailer
+    default :from => "ejwehder@gmail.com"
+
+    def password_reset(user)
+        @user = user
+        @code = generate_password_code
+        mail(:to => user.email, :subject => "Forgot your Password")
+    end
+
+    def generate_password_code
+        # create a code using SecureRandom
+        # update the column in the DB for the user's password reset
+        code = SecureRandom.hex(6)
+
+        @user.update(password_reset_code: code)
+
+        code
+    end
+
+end
