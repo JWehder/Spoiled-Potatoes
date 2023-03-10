59efdcb839ea428cf3e7b1bf4778f0c7cb346dc4
Jake Wehder <jw@email.comjake>
2023-03-09 19:16:00 -0600

    adding a get route to get information about the logged in user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: render_unprocessable_entity
+
+    def create
+        user = User.create!(user_params)
+        render json: user, status: :created
+    end
+
+    def show
+
+    private
+
+    def render_record_invalid_response(invalid)
+        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
+
+    def user_params
+        params.permit(:first_name, :last_name, :username, :email, :password, :password_confirmation)
+    end
+
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+    has_secure_password
+
+    validates :username, presence: true, uniqueness: true
+    validates :password, presence: true, length: { minimum: 8 }, format: { with: /\A(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/x }
+    validates :email, presence: true,
+        format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i/x }
+        uniqueness: { case_sensitive: false } 
+    validates :password_confirmation, presence: true
+
+end
