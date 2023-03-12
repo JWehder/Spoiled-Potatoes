89834354413942f0c34af5b52a09d1c21d2dc56a
Jake Wehder <jw@email.comjake>
2023-03-12 15:55:54 -0500

    adding a password confirmation input field. making each input field controlled

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,39 +1,42 @@
 import React from "react";
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import StyledForm from "../styles/StyledForm";
+import CustomButton from "../styles/Button";
 
 function SignupForm() {
     return (
         <>  
-            <FloatingLabel
-            controlId="floatingInput"
-            label="Username"
-            className="mb-3"
-            >
-            <StyledForm.Control type="text" placeholder="Username" />
-            </FloatingLabel>
-            <FloatingLabel controlId="floatingPassword" label="Password">
-            <StyledForm.Control type="password" placeholder="Password" />
-            </FloatingLabel>
-            <FloatingLabel
-            controlId="floatingInput"
-            label="Email address"
-            className="mb-3"
-            >
-            <StyledForm.Control type="email" placeholder="name@example.com" />
-            </FloatingLabel>  
-            <FloatingLabel
-            controlId="floatingInput"
-            label="Favorite Movie"
-            className="mb-3"
-            >
-            <StyledForm.Control type="text" placeholder="Favorite Movie" />
-            </FloatingLabel>
-            <StyledForm.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <StyledForm.Label>Example textarea</StyledForm.Label>
-            <StyledForm.Control as="textarea" rows={3} />
-            </StyledForm.Group>
-
+            <Form >
+                <FloatingLabel
+                controlId="floatingInput"
+                label="Username"
+                className="mb-3"
+                >
+                <StyledForm.Control type="text" placeholder="Username" />
+                </FloatingLabel>
+                <FloatingLabel controlId="floatingPassword" label="Password">
+                <StyledForm.Control type="password" placeholder="Password" />
+                </FloatingLabel>
+                <FloatingLabel
+                controlId="floatingInput"
+                label="Email address"
+                className="mb-3"
+                >
+                <StyledForm.Control type="email" placeholder="name@example.com" />
+                </FloatingLabel>  
+                <FloatingLabel
+                controlId="floatingInput"
+                label="Favorite Movie"
+                className="mb-3"
+                >
+                <StyledForm.Control type="text" placeholder="Favorite Movie" />
+                </FloatingLabel>
+                <StyledForm.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <StyledForm.Label>Example textarea</StyledForm.Label>
+                <StyledForm.Control as="textarea" rows={3} />
+                </StyledForm.Group>
+                <CustomButton variant= "primary" type="submit" />
+            </Form>
         </>
     )
 }
