83abd27a00cc9ce54b98c5dfe359a1e91c620690
Jake Wehder <jw@email.comjake>
2023-03-11 19:17:43 -0600

    building styles folder and a styled button component to use throughout my project

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom"
+
 
 function App() {
   return (
     <div className="App">
 
       <button>Click me!</button>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+      </Switch>
 
     </div>
   );
