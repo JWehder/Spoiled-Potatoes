c559b3e1241e07c4fb3fd406415489f23b4e2599
Jake Wehder <jw@email.comjake>
2023-03-13 11:55:46 -0500

    adding in an error component to show errors on the client side to the user

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,21 +1,62 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import StyledForm from "../styles/StyledForm";
 import CustomButton from "../styles/Button";
+import { UserProvider } from "../context/User";
 
 function LoginForm() {
+    const { setUser } = useContext(UserProvider)
+
+    const [loginUser, setLoginUser] = useState({
+        username: "",
+        password: ""
+    })
+    const [errors, setErrors] = useState([])
+
+    function onChange(e) {
+        setLoginUser({
+            ...loginUser,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    function handleSubmit() {
+        fetch('/login', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(loginUser),
+        }).then((r) => {
+            if (r.ok) {
+                r.json().then((user) => setUser(user))
+            } else {
+                r.json.then((err) => setErrors(err.errors))
+            }
+        })
+    }
+
     return (
         <>
-            <StyledForm>
+            <StyledForm onSubmit={handleSubmit}>
                 <FloatingLabel
                 controlId="floatingInput"
                 label="Username"
                 className="mb-3"
                 >
-                <StyledForm.Control type="text" placeholder="name@example.com" />
+                <StyledForm.Control 
+                type="text" 
+                placeholder="Username" 
+                value= {loginUser}
+                onChange={(e) => onChange(e)}
+                />
                 </FloatingLabel>
                 <FloatingLabel controlId="floatingPassword" label="Password">
-                <StyledForm.Control type="password" placeholder="Password" />
+                <StyledForm.Control 
+                type="password" 
+                placeholder="Password" 
+                onChange={(e) => onChange(e)}
+                />
                 </FloatingLabel>
                 <CustomButton variant="primary" type="submit" />
             </StyledForm>
