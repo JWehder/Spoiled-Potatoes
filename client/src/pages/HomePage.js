24c853207d67c9d39eb4ce4f19593a20b69a34b4
Jake Wehder <jw@email.comjake>
2023-03-21 18:35:20 -0500

    creating a fetch request to my rails DB to grab the movies

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import MovieCard from "../components/MovieCard";
 
 function HomePage() {
+
+    const [movies, setMovies] = useState([])
 
     
 
