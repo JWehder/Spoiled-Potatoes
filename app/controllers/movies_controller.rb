6b13a5e9db58d65951fbabee5b99dce90ebfb138
Jake Wehder <jw@email.comjake>
2023-03-21 10:14:27 -0500

    creating movie data to fit my database manually

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,5 @@
 class MoviesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
     def index
         movies = Movie.all
@@ -6,6 +7,14 @@ class MoviesController < ApplicationController
     end
 
     def show
-        
+        movie = movie.find(params[:title])
+        render json: movie, status: :ok
     end
+
+    private
+
+    def render_not_found_response
+        render json: { "Movie not found" }, status: :not_found
+    end
+
 end
