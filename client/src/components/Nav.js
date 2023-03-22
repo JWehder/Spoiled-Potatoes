0e4dabc13117c9ff1ca0f906204a107548750521
Jake Wehder <jw@email.comjake>
2023-03-22 18:33:52 -0500

    building a submit event for my search bar form and then a method to query the DB in the backend

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,28 +1,35 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import StyledForm from "../styles/StyledForm";
 import styled from "styled-components";
+import { UserContext } from "../context/User";
 
 function Navigation() {
+    const [search, setSearch] = useState("")
+    const { user, setUser } = useContext(UserContext)
+
     return (
         <>
             <Navbar bg="light" variant="light">
                 <Container>
                 <Navbar.Brand href="#home">Navbar</Navbar.Brand>
                 <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Movies</Nav.Link>
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/movies">Movies</Nav.Link>
                 </Nav>
                 <div style={{ display: 'flex', justifyContent: 'center'}}>
                 <SearchBar
                     type="text" 
                     name="Search"
                     placeholder="Search for a movie..."
-                    // value={search}
-                    // onChange={(e) => changeUserValue(e)}
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
                 />
                 </div>
+                <Nav >
+                    <Nav.Link href="/login">Logout</Nav.Link>
+                </Nav>
                 </Container>
             </Navbar>
         </>
