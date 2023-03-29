916880e0ce44c1d5852be64c28c9409c3a3df064
Jake Wehder <jw@email.comjake>
2023-03-29 11:55:02 -0500

    creating a new component for the comment section below each movie

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
-    
+    belongs_to :movie
+    belongs_to :user
+
+    validates :rating, presence: true
+    validates :comment, presence: true, length: { minimum: 10 }
 end
