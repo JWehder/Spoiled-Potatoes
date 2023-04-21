75e28992c95a8e34dba16311cf3e21269fb9ff9e
Jake Wehder <jw@email.comjake>
2023-04-21 17:03:07 -0500

    getting rid of comments over validations in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,15 @@ class User < ApplicationRecord
 
     has_secure_password
     validates :username, presence: true, uniqueness: true, length: { maximum: 25 }
-    # validates :first_name, presence: true
-    # validates :last_name, presence: true
-    # validates :password, length: { minimum: 8 }, format: { with: /\A(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/x }
-    # validates :email, presence: true,
-    #     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
-    #     uniqueness: { case_sensitive: false }
-    # validates :password_confirmation, presence: true
-    # validates :favorite_movie, presence: true
-    # validates :bio, presence: true, length: { minimum: 50 }
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :password, length: { minimum: 8 }, format: { with: /\A(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/x }
+    validates :email, presence: true,
+        format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+        uniqueness: { case_sensitive: false }
+    validates :password_confirmation, presence: true
+    validates :favorite_movie, presence: true
+    validates :bio, presence: true, length: { minimum: 50 }
 
     def unique_movies
         if (self.movies)
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -122,5 +122,4 @@ Rails.application.configure do
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
 
-
 end
